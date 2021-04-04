b9577df7c5fefb8ed2ae06d23b44d1c3db16f2be
Andrew Ballinger <andrew@Andrews-MacBook-Air.local>
2021-04-04 17:38:35 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
