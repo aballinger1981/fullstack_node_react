27394f1b4833f9f428448941974f1c97078ce882
Andrew Ballinger <andrew@Andrews-MacBook-Air.local>
2021-05-16 23:23:59 -0400

    move survey list into separate method

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -7,30 +7,33 @@ class SurveyList extends Component {
     this.props.fetchSurveys();
   }
 
+  renderSurveys() {
+    return this.props.surveys.reverse().map((survey) => (
+      // eslint-disable-next-line no-underscore-dangle
+      <div className="card darken-1 blue-grey" key={survey._id}>
+        <div className="card-content white-text">
+          <span className="card-title">{survey.title}</span>
+          <p>
+            {survey.body}
+          </p>
+          <p className="right">
+            Sent On: {new Date(survey.dateSent).toLocaleDateString()}
+          </p>
+        </div>
+        <div className="card-action">
+          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+          <a>Yes: {survey.yes}</a>
+          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+          <a>No: {survey.no}</a>
+        </div>
+      </div>
+    ));
+  }
+
   render() {
     return (
       <div>
-        {(console.log(this.props.surveys))}
-        {(this.props.surveys.map((survey) => (
-          // eslint-disable-next-line no-underscore-dangle
-          <div className="card darken-1 blue-grey" key={survey._id}>
-            <div className="card-content white-text">
-              <span className="card-title">{survey.title}</span>
-              <p>
-                {survey.body}
-              </p>
-              <p className="right">
-                Sent On: {new Date(survey.dateSent).toLocaleDateString()}
-              </p>
-            </div>
-            <div className="card-action">
-              {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-              <a>Yes: {survey.yes}</a>
-              {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-              <a>No: {survey.no}</a>
-            </div>
-          </div>
-        )))}
+        {this.renderSurveys()}
       </div>
     );
   }
