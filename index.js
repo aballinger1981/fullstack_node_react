e9ecfa38900a273ad78e2e8574b829badf34bcf4
Andrew Ballinger <andrew@Andrews-MacBook-Air.local>
2021-05-24 14:53:42 -0400

    edit express static location

--- index.js
+++ index.js
@@ -43,9 +43,8 @@ import surveyRoutes from './routes/surveyRoutes';
   if (process.env.NODE_ENV === 'production') {
     // eslint-disable-next-line no-underscore-dangle
     const __dirname = dirname(fileURLToPath(import.meta.url));
-    console.log('dirname', __dirname);
     // Express will serve up production assets like our main.js or main.css file
-    app.use(express.static(resolve(__dirname, './client/build')));
+    app.use(express.static('client/build'));
     // Express will serve up the index.html file if it doesn't recognize the route
     // eslint-disable-next-line global-require
     app.get('*', (req, res) => {
