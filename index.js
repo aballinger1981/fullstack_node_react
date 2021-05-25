b5cdb191d0d4351d9687c821ac1ca81025e6a386
Andrew Ballinger <andrew@Andrews-MacBook-Air.local>
2021-05-25 14:13:28 -0400

    fix routes

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ import surveyRoutes from './routes/surveyRoutes';
     app.use(express.static('client/build'));
     // Express will serve up the index.html file if it doesn't recognize the route
     // eslint-disable-next-line global-require
-    app.get('*', (req, res) => {
+    app.get('/*', (req, res) => {
       res.sendFile(resolve(__dirname, './client/build', 'index.html'));
     });
   }
