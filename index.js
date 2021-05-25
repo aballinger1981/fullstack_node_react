39b670d6d327503987c5cb45ae9dba54b2d00c04
Andrew Ballinger <andrew@Andrews-MacBook-Air.local>
2021-05-25 15:20:30 -0400

    fix routes

--- index.js
+++ index.js
@@ -40,19 +40,14 @@ import surveyRoutes from './routes/surveyRoutes';
   billingRoutes(app);
   surveyRoutes(app);
 
-  console.log('env', process.env.NODE_ENV);
   if (process.env.NODE_ENV === 'production') {
-    console.log('HELLO');
-    console.log('fileURL', fileURLToPath(import.meta.url));
     // eslint-disable-next-line no-underscore-dangle
     const __dirname = dirname(fileURLToPath(import.meta.url));
-    console.log('dirname', __dirname);
     // Express will serve up production assets like our main.js or main.css file
     app.use(express.static('client/build'));
     // Express will serve up the index.html file if it doesn't recognize the route
     // eslint-disable-next-line global-require
     app.get('*', (req, res) => {
-      console.log('index.html');
       res.sendFile(resolve(__dirname, './client/build', 'index.html'));
     });
   }
